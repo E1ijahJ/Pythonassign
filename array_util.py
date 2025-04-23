d78bccbc1e946c716460170474c381e0057760c5
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-22 23:27:51 -0400

    Added the echo string funciton

--- array_util.py
+++ array_util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import subprocess
 
 def average_3d_chunks(arr):
     x, y, z = arr.shape
@@ -11,3 +12,9 @@ def average_3d_chunks(arr):
                 chunk = arr[i:i + x // 2, j:j + y // 2, k:k + z // 2]
                 chunks.append(np.mean(chunk))
     return chunks
+
+def echo_string(string):
+    for s in string:
+        subprocess.run(['echo'],s)
+
+
